485173ead8d14b9776f5468bdd7945deb4f08f01
Lokeek Lokhande <lokeeklokhande1185@gmail.com>
2024-08-21 13:54:33 +0530

    Create sum_mean_prod.r

---
+++ sum_mean_prod.r
@@ -0,0 +1,9 @@
+numbers <- c(1, 2, 3, 4, 5)
+
+sum_numbers <- sum(numbers)
+mean_numbers <- mean(numbers)
+product_numbers <- prod(numbers)
+
+print(paste("Sum:", sum_numbers))
+print(paste("Mean:", mean_numbers))  
+print(paste("Product:", product_numbers))
